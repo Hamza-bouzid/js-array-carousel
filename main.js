822359712f5be4377af4cf4f39d683e51bf5e815
Hamza Bouzid <hamzadeveloper1@gmail.com>
2021-12-15 10:08:06 +0100

    completato

--- main.js
+++ main.js
@@ -26,6 +26,7 @@ for (let i = 0; i < items.length; i++) {
 for (let i = 0; i < items.length; i++) {
   miniItems += `
   <div class="mini-slide-item" id="mini-${i}">
+   <div class="overlay" id="overlay-${i}"></div>
    <img src="${items[i]}" />
   </div>
   `;
@@ -36,6 +37,63 @@ miniSlide.innerHTML += miniItems;
 
 let carouselActive = document.getElementById("item-" + elementActive);
 carouselActive.classList.add("full-active");
-
 let miniActive = document.getElementById("mini-" + elementActive);
 miniActive.classList.add("mini-active");
+let overlayActive = document.getElementById("overlay-" + elementActive);
+overlayActive.classList.add("overlay-none");
+
+const arrowUp = document.querySelector(".arrow-up");
+const arrowDown = document.querySelector(".arrow-down");
+
+// Evento Di click su Buttone Down
+arrowDown.addEventListener("click", function () {
+  elementActive++;
+  if (elementActive > 4) {
+    elementActive = 0;
+  }
+
+  if (miniActive.classList.contains("mini-active")) {
+    miniActive.classList.toggle("mini-active");
+  }
+
+  if (overlayActive.classList.contains("overlay-none")) {
+    overlayActive.classList.toggle("overlay-none");
+  }
+
+  if (carouselActive.classList.contains("full-active")) {
+    carouselActive.classList.toggle("full-active");
+  }
+
+  carouselActive = document.getElementById("item-" + elementActive);
+  carouselActive.classList.add("full-active");
+  miniActive = document.getElementById("mini-" + elementActive);
+  miniActive.classList.add("mini-active");
+  overlayActive = document.getElementById("overlay-" + elementActive);
+  overlayActive.classList.add("overlay-none");
+});
+
+// Evento Di click su Buttone UP
+arrowUp.addEventListener("click", function () {
+  elementActive--;
+  if (elementActive < 0) {
+    elementActive = 4;
+  }
+
+  if (miniActive.classList.contains("mini-active")) {
+    miniActive.classList.toggle("mini-active");
+  }
+
+  if (overlayActive.classList.contains("overlay-none")) {
+    overlayActive.classList.toggle("overlay-none");
+  }
+
+  if (carouselActive.classList.contains("full-active")) {
+    carouselActive.classList.toggle("full-active");
+  }
+  carouselActive = document.getElementById("item-" + elementActive);
+  carouselActive.classList.add("full-active");
+  miniActive = document.getElementById("mini-" + elementActive);
+  miniActive.classList.add("mini-active");
+  overlayActive = document.getElementById("overlay-" + elementActive);
+  overlayActive.classList.add("overlay-none");
+});
